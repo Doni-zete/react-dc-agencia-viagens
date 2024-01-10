1e9a6694a7091c9fb3da082b393e5d8a7707f327
doni-zete <donizete112@gmail.com>
2024-01-09 23:23:59 -0300

    feat: Adicionando componentes ao app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import Rodape from "./components/Rodape";
+import SecaoBanner from "./components/SecaoBanner";
+import SecaoExperienciaTrabalho from "./components/SecaoExperienciaTrabalho";
+import Topo from "./components/Topo";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Topo />
+      <SecaoBanner />
+      <SecaoExperienciaTrabalho />
+      <Rodape/>
+    </main>
+
   );
 }
 
