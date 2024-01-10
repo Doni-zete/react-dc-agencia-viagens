4b66933a8cc6c858bcb93aeb473cfe2a801d538f
doni-zete <donizete112@gmail.com>
2024-01-09 23:23:27 -0300

    feat: Adicionando className

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,9 +1,11 @@
+import './style.css'
+
 export default function Card(props) {
  return (
-  <div>
-   <p>{props.data}</p>
+  <div id="card" className="card-claro">
+   <p className="primeiro-paragrafo">{props.data}</p>
    <h4>{props.titulo}</h4>
-   <p>{props.empresa}</p>
+   <p className="primeiro-segundo">{props.empresa}</p>
    <p>{props.paragrafo}</p>
   </div>
  )
