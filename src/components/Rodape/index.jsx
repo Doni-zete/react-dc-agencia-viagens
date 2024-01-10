d84bd29c0a2d738a3f0f61068e91eaf8cafcbe44
doni-zete <donizete112@gmail.com>
2024-01-09 23:30:13 -0300

    feat: Adicionando estrutura html

--- src/components/Rodape/index.jsx
+++ src/components/Rodape/index.jsx
@@ -6,23 +6,32 @@ import iconeDribe from '../../assets/dribble.png'
 import iconeBehance from '../../assets/behance.png'
 import iconeGoogle from '../../assets/google-plus.png'
 
+import './style.css'
+
 export default function Rodape() {
  return (
-  <footer>
+  <footer className="rodape-claro">
    <img src={Logo} alt="logoempresa" />
-   <img src={iconeFacebook} alt="icone facebook" />
-   <img src={iconeTwiter} alt="icone twwitter" />
-   <img src={iconeLinkedin} alt="icone linkedin" />
-   <img src={iconeDribe} alt="icone Dribe" />
-   <img src={iconeBehance} alt="icone behance" />
-   <img src={iconeGoogle} alt="icone google" />
-   <p>
+
+   <p className="paragrafo">
     Ajudamos a criar uma personalidade digital construindo sua marca no ambiente
     online utilizando estratégias, ferramentas e tecnologias personalizadas.
    </p>
 
-   <p>
-    Copyright 2024 <span>Donizete Crisostomo</span>
+   <div className="iconesRodape">
+    <img src={iconeFacebook} alt="icone facebook" />
+    <img src={iconeTwiter} alt="icone twwitter" />
+    <img src={iconeLinkedin} alt="icone linkedin" />
+    <img src={iconeDribe} alt="icone Dribe" />
+    <img src={iconeBehance} alt="icone behance" />
+    <img src={iconeGoogle} alt="icone google" />
+   </div>
+
+   <p className="copyright">
+    Copyright 2024 &copy;
+    <a href="https://github.com/Doni-zete" target="_blank" rel="noreferrer">
+     <span>Donizete Crisostomo</span>{' '}
+    </a>
    </p>
   </footer>
  )
