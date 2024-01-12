05d9dc8a8ccab43038d57d2b86baba439a2a1ed4
doni-zete <donizete112@gmail.com>
2024-01-12 09:56:05 -0300

    feat: Aplicando useStae

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import SecaoBanner from "./components/SecaoBanner";
 import SecaoExperienciaTrabalho from "./components/SecaoExperienciaTrabalho";
 import Topo from "./components/Topo";
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [temaEscuro, setTemaEscuro] = useState(false)
+
+  const alterarCor = () => {
+    setTemaEscuro(!temaEscuro)
+  }
+
   return (
     <main>
-      <Topo />
-      <SecaoBanner />
-      <SecaoExperienciaTrabalho />
-      <Rodape/>
+      <Topo alterarCor={alterarCor} temaEscuro={temaEscuro} />
+      <SecaoBanner temaEscuro={temaEscuro} />
+      <SecaoExperienciaTrabalho temaEscuro={temaEscuro} />
+      <Rodape temaEscuro={temaEscuro} />
     </main>
 
   );
