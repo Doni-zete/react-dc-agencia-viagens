5774c957aaff5b81ec7f7483d8f018b408a65372
doni-zete <donizete112@gmail.com>
2024-01-12 10:06:48 -0300

    feat: Adicionando logo sol e aplicando props

--- src/components/Topo/index.jsx
+++ src/components/Topo/index.jsx
@@ -1,15 +1,22 @@
 import Logo from '../../assets/logo.png'
 import IconeLua from '../../assets/moon.png'
-
+import IconeSol from '../../assets/sun.png'
 import './style.css'
 
-export default function Topo() {
+export default function Topo(props) {
  return (
-  <header className="topo-modo-claro">
+  <header className={props.temaEscuro ? 'topo-modo-escuro' : 'topo-modo-claro'}>
    <img className="logo" src={Logo} alt="logoimagem" />
 
-   <button className="botao-claro">
-    <img className="icone" src={IconeLua} alt="iconeLua" />
+   <button
+    onClick={props.alterarCor}
+    className={props.temaEscuro ? 'botao-escuro' : 'botao-claro'}
+   >
+    <img
+     className="icone"
+     src={props.temaEscuro ? IconeSol : IconeLua}
+     alt="iconeSol"
+    />
    </button>
   </header>
  )
